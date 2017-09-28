8fa23675e175425760cf9e14544c42a9720983bc
Athanasios Gotsopoulos <tanilas@hotmail.com>
2017-09-28 15:00:28 +0300

    Trying with sparse matrix

---
+++ indexed_multidimensional.m
@@ -0,0 +1,52 @@
+%% This script reads the MIDI, analyzes it and generates a new composition
+clear
+addpath('./src') % Importing the MATLAB MIDI toolbox by Ken Schutte (http://kenschutte.com/midi)
+%addpath('./mc_files')
+addpath('./functions') %
+prefix='./original_midis/'; % Where the original midis are stored (that are used for the analysis)
+
+%% Reading files
+filename='furelise.mid'
+midi=readmidi([prefix filename]);
+
+Notes = midiInfo(midi,0);% Take all information from the MIDI file.
+
+num_of_notes=size(Notes,1); % Find the number of notes (note ons) in the midi file
+
+%% Get the information from the MIDIS
+channel=0
+track=2;
+Notes=Notes(Notes(:,1)==track,:);
+Notes=Notes(Notes(:,2)==channel,:);
+data=Notes(1:num_of_notes,3:6);
+
+durs=data(:,4)-data(:,3);
+%% Find notes that start at the same time to generate patterns
+s=size(data);
+stp=data(:,3);
+notes=data(:,1);
+unotes=unique(notes);
+nnotes=notes-min(notes)+1; % To find the range of notes (e.g. from note 32 to note 56)
+curpat=zeros(max(nnotes),1); %% Make an empty vector to add the patterns
+pats=[]; 
+patdurs=[];
+curpat(nnotes(1))=1;
+
+for i=2:s(1)
+    if stp(i-1)~=stp(i)
+       pats=[pats curpat];
+       
+       curpat=zeros(max(nnotes),1);
+    end
+    curpat(nnotes(i))=1;
+end
+
+%%
+
+
+musiclen=1000;
+
+[ub,u,bb]=unique(pats','rows');
+imagesc(ub'); ylabel('Notes'); xlabel('Pattern index')
+display(['Number of different patterns: ' num2str(max(bb))])
+
